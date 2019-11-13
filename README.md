a9a200d000603ef6ecd55014f7ce1082e457f448
patriciasmashp <egor.yahin@mail.ru>
2019-11-13 23:31:50 +0500

    deploy

---
+++ README.md
@@ -0,0 +1,2 @@
+"# My_rep" 
+"# HabrParser_Bot" 
---
+++ pars.py
@@ -0,0 +1,72 @@
+import requests 
+from bs4 import BeautifulSoup as bs 
+import datetime
+headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+'Accept-Encoding': 'gzip, deflate',
+'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+'Cache-Control': 'max-age=0',
+'Connection': 'keep-alive',
+'Cookie': '_ym_uid=156587604930542632; _ym_d=1565876049; a05e2afef66f287301157ede30e3e523=8befirnn4jlmd1j30p4ahv0m99; _ym_isad=1',
+'Host': 'ntgmk.ru',
+'Referer': 'https://www.google.com/',
+'Upgrade-Insecure-Requests': '1',
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}
+
+b_url = 'http://ntgmk.ru/program/view_zamen.php'
+
+session = requests.Session()
+request = session.get(b_url, headers=headers)
+replace = []
+res = []
+date = []
+def find_repl_753(b):
+	d = None
+	for i in b:
+		if i is not None:
+			d = i.findAll('td',attrs={'colspan':'7','height':'18','bgcolor':"#f6f6f6",'class':"tbl_c"})
+			if len(d)>0:
+				date.append(d)
+	for tag in b: #b- all_soup
+		a=(tag.find('td',attrs={'height':"12",'class':"tbl_c"}))
+		if tag.text[6:8] == date[1][0].text[6:8]:
+			
+			break
+
+		if a is not None :
+			if a.text[-2] == '3' and a.text[-3] == '5' and a.text[-4] == '7':
+				replace.append(a.parent)
+def decor(func):
+	def wrapper():
+		print('Замены на {0}:'.format(date[0][0].text[6:8]))
+		resoult= func()
+		return resoult
+	return wrapper
+@decor
+def Out_repl():
+	res = []
+	for i in replace:
+		res.append(i.text.replace('\n',''))
+	return res
+				
+
+
+def resoult():
+	if request.status_code == 200:
+		soup = bs(request.content, 'lxml')
+		body_tag = soup.table
+	
+		all_soup = body_tag.findAll()	
+		find_repl_753(all_soup)
+		return (Out_repl())
+
+
+
+
+	# for i in body_tag.findAll('td',attrs={'colspan':'7','height':'18','bgcolor':"#f6f6f6",'class':"tbl_c"}):
+	# 	date.append(i)
+	# if int(datetime.date.today().strftime('%d')) <	int(date[0].text[6:8]):
+		# print( date[0].text[6:8] )
+
+
+	
+
